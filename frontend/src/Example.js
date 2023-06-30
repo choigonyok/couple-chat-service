171a0dfe11f2f81f151f659cbb170fbd15123bdd
choigonyok <achoistic98@naver.com>
2023-07-01 05:12:52 +0900

    BUILD : REACT, DOCKER_COMPOSE 환경변수 설정

--- frontend/src/Example.js
+++ frontend/src/Example.js
@@ -6,9 +6,11 @@ const Example = () => {
   const [test, setTest] = useState();
   const [data, setData] = useState([]);
 
+  const HOST_URL = process.env.HOST_URL
+
   useEffect(() => {
     axios
-      .get("http://localhost/api/test")
+      .get(HOST_URL+"/api/test")
       // api호출은 go port num인 8080이 아니라 container port num인 1000으로 요청해야 통신이 됨
       // localhost:8080으로 요청하면 통신 안됨
       .then((response) => {
@@ -19,7 +21,7 @@ const Example = () => {
       });
 
     axios
-      .get("http://localhost/api/usr")
+      .get(HOST_URL+"/api/usr")
       .then((response) => {
         setData(response.data);
         // 서버에서 보낼 때 Marshaling으로 JSON 형식 인코딩을 해서 보냈기 때문에

3b46e13f8d55086f45965d574ac9ab3b4310dc36
choigonyok <achoistic98@naver.com>
2023-07-03 02:39:53 +0900

    FEAT : ID중복/폼 미작성에 따른 사용자 피드백 UI 구현

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,65 +1,101 @@
 import axios from "axios";
 import { useState } from "react";
 
-
 const Signup = () => {
-        const [inputID, setInputID] = useState("");
-        const [inputPW, setInputPW] = useState("");
-        const [isIDChecked, setIsIDChecked] = useState(false);
+  const [inputID, setInputID] = useState("");
+  const [inputPW, setInputPW] = useState("");
+  const [isIDChecked, setIsIDChecked] = useState(false);
+  const [isAlready, setIsAlready] = useState(false);
 
+  const inputIDHandler = (e) => {
+    setInputID(e.target.value);
+    setIsIDChecked(false);
+    setIsAlready(false);
+    // 아이디 중복 확인을 위해서 작성값이 바뀌면 다시 중복 검사를 하도록 함
+  };
 
-        const inputIDHandler = (e) => {
-                setInputID(e.target.value);
-                setIsIDChecked(false);
-                // 아이디 중복 확인을 위해서 작성값이 바뀌면 다시 중복 검사를 하도록 함
-        }
+  const inputPWHandler = (e) => {
+    setInputPW(e.target.value);
+  };
 
-        const inputPWHandler = (e) => {
-                setInputPW(e.target.value);
-        }
+  const checkIDHandler = () => {
+    const data = {
+      input_id: inputID,
+    };
+    axios
+      .post(process.env.REACT_APP_HOST_URL + "/api/id", JSON.stringify(data))
+      .then((response) => {
+        console.log("중복된 아이디 없음. 사용가능");
+        setIsIDChecked(true);
+      })
+      .catch((error) => {
+        setIsAlready(true);
+      });
+  };
 
-        const checkIDHandler = () => {
-                const data = {
-                        input_id : inputID,
-                }
-                axios
-                        .post(process.env.REACT_APP_HOST_URL+"/api/id", JSON.stringify(data))
-                        .then((response)=>{
-                                console.log("중복된 아이디 없음. 사용가능");
-                                setIsIDChecked(true);
-                        })
-                        .catch((error)=>{
-                                console.log("아이디 중복 확인 에러 발생");
-                        })
-        }
+  const signUpHandler = () => {
+    if (inputID === "" || inputPW === "") {
+      alert("ID 혹은 PASSWORD가 작성되지 않았습니다.");
+    } else if (isIDChecked === false && isAlready === false) {
+      alert("ID 중복 확인을 해주세요.");
+    } else if (isIDChecked === false && isAlready) {
+      alert("이미 사용중인 아이디입니다.");
+    } else {
+      const usrData = {
+        usr_id: inputID,
+        usr_pw: inputPW,
+      };
+      axios
+        .post(process.env.REACT_APP_HOST_URL + "/api/usr", usrData)
+        .then((response) => {
+          alert("회원가입이 완료되었습니다.");
+          setInputID("");
+          setInputPW("");
+          setIsIDChecked(false);
+        })
+        .catch((error) => {
+          alert("회원가입에 실패했습니다.");
+          setInputID("");
+          setInputPW("");
+          setIsIDChecked(false);
+        });
+    }
+  };
 
-        const signUpHandler = () => {
-                if (inputID === "" || inputPW === "") {
-                        alert("ID 혹은 PASSWORD가 작성되지 않았습니다.");
-                } else if (isIDChecked === false) {
-                        alert("ID 중복 확인을 해주세요.");
-                } else {
-                        const usrData = {
-                                usr_id : inputID,
-                                usr_pw : inputPW,
-                        }
-                        axios
-                        .post(process.env.REACT_APP_HOST_URL+"/api/usr", usrData)
-                        .then((response)=> {
-                                alert("회원가입이 완료되었습니다.");
-                        })
-                        .catch((error)=>{
-                                alert("회원가입에 실패했습니다.")
-                        })
-                };
-        }
-
-        return <div>
-                <input type="text" placeholder="ID" value={inputID} onChange={inputIDHandler}/>
-                <input type="button" value="중복 확인" onClick={checkIDHandler}/>
-                <input type="password" placeholder="PASSWORD" value={inputPW} onChange={inputPWHandler}/>
-                <input type="button" value="가입하기" onClick={signUpHandler}/>
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="ID"
+          value={inputID}
+          onChange={inputIDHandler}
+        />
+        <input type="button" value="중복 확인" onClick={checkIDHandler} />
+      </div>
+      {isIDChecked && (
+        <div>
+          <h5>사용 가능한 아이디 입니다.</h5>
         </div>
+      )}
+      {isAlready && (
+        <div>
+          <h5>사용중인 아이디 입니다.</h5>
+        </div>
+      )}
+      <div>
+        <input
+          type="password"
+          placeholder="PASSWORD"
+          value={inputPW}
+          onChange={inputPWHandler}
+        />
+      </div>
+      <div>
+        <input type="button" value="가입하기" onClick={signUpHandler} />
+      </div>
+    </div>
+  );
 };
 
 export default Signup;

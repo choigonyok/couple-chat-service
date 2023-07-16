c6fee4da514f4148234a8bd05fa734a4ea2613ac
choigonyok <achoistic98@naver.com>
2023-07-17 01:31:24 +0900

    FEAT : 일정 입력 UI/UX 구현

--- frontend/src/Calender.js
+++ frontend/src/Calender.js
@@ -15,6 +15,8 @@ const Calender = () => {
   date2.setMonth(month + 1); // date1 = 07/01
   const [dateArray, setDateArray] = useState([]);
   const [weeksArray, setWeeksArray] = useState([]);
+  const [dateInfo, setDateInfo] = useState(0);
+  const [inputAnniversary, setInputAnniversary] = useState("");
 
   let firstWeeksLastDate = 7 - date1.getDay();
   let lastDateOfThisMonth = date2.getDate(date2.setDate(date2.getDate() - 1));
@@ -24,8 +26,6 @@ const Calender = () => {
     weeksOfThisMonth = i;
   }
   weeksOfThisMonth += 2;
-
-  console.log(date1.getDay());
 
   useEffect(() => {
     const array = [];
@@ -51,10 +51,11 @@ const Calender = () => {
   }, [month]);
 
   const prevMonthHandler = () => {
-    if (month % 12 === 0 ) {
+    if (month % 12 === 0) {
       setYear(year - 1);
     }
     setMonth(month - 1);
+    setDateInfo(0);
   };
 
   const nextMonthHandler = () => {
@@ -62,19 +63,70 @@ const Calender = () => {
       setYear(year + 1);
     }
     setMonth(month + 1);
+    setDateInfo(0);
+    setInputAnniversary("");
   };
 
   const setTodayHanndler = () => {
     setMonth(thisMonth);
     setYear(thisYear);
-  }
+    setDateInfo(0);
+    setInputAnniversary("");
+  };
+
+  const dateClickHandler = (value) => {
+    setDateInfo(value);
+  };
+
+  const deleteBoxHandler = () => {
+    setDateInfo(0);
+    setInputAnniversary("");
+  };
+
+  const sendAnniversaryHandler = () => {
+    setInputAnniversary("");
+    setDateInfo(0);
+    console.log("TARGET YEAR : ", year);
+    const monthInfo = month + 1 <= 0 ? ((month + 1) % 12) + 12 : (month % 12) + 1
+    console.log("TARGET MONTH : ", monthInfo);
+    console.log("TARGET DATE : ", dateInfo);
+    console.log("TARGET TEXT : ", inputAnniversary);
+  };
+
+  const inputAnniversaryHandler = (e) => {
+    setInputAnniversary(e.target.value);
+  };
 
   return (
     <div>
       <div>
         <div>
-          {year}년 {month+1 <= 0 ? (month+1) % 12 +12 : month % 12 + 1}월
+          {year}년 {month + 1 <= 0 ? ((month + 1) % 12) + 12 : (month % 12) + 1}
+          월
         </div>
+        {dateInfo !== 0 && (
+          <div>
+            <div>
+              {year}/
+              {month + 1 <= 0 ? ((month + 1) % 12) + 12 : (month % 12) + 1}/
+              {dateInfo}
+            </div>
+            <div>
+              <input
+                type="text"
+                value={inputAnniversary}
+                onChange={inputAnniversaryHandler}
+                placeholder="일정을 입력해주세요"
+              />
+              <input
+                type="button"
+                value="일정 저장하기"
+                onClick={sendAnniversaryHandler}
+              />
+              <input type="button" value="X" onClick={deleteBoxHandler} />
+            </div>
+          </div>
+        )}
         <div>
           <input type="button" value="prev" onClick={prevMonthHandler} />
           <input type="button" value="today" onClick={setTodayHanndler} />
@@ -92,25 +144,74 @@ const Calender = () => {
           </div>
           {weeksArray.map((item, index) => (
             <div className="calender-container">
-              <div className={thisMonth === month && dateArray[index * 7 + 0] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 0] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 0])}
+              >
                 {dateArray[index * 7 + 0] !== 0 && dateArray[index * 7 + 0]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 1] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 1] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 1])}
+              >
                 {dateArray[index * 7 + 1] !== 0 && dateArray[index * 7 + 1]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 2] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 2] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 2])}
+              >
                 {dateArray[index * 7 + 2] !== 0 && dateArray[index * 7 + 2]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 3] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 3] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 3])}
+              >
                 {dateArray[index * 7 + 3] !== 0 && dateArray[index * 7 + 3]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 4] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 4] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 4])}
+              >
                 {dateArray[index * 7 + 4] !== 0 && dateArray[index * 7 + 4]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 5] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 5] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 5])}
+              >
                 {dateArray[index * 7 + 5] !== 0 && dateArray[index * 7 + 5]}
               </div>
-              <div className={thisMonth === month && dateArray[index * 7 + 6] === thisDate ? "date__today" : "date"}>
+              <div
+                className={
+                  thisMonth === month && dateArray[index * 7 + 6] === thisDate
+                    ? "date__today"
+                    : "date"
+                }
+                onClick={() => dateClickHandler(dateArray[index * 7 + 6])}
+              >
                 {dateArray[index * 7 + 6] !== 0 && dateArray[index * 7 + 6]}
               </div>
             </div>

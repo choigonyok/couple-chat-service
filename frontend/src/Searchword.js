c10a1c029dbd1074f33dcf9c167cf6f4c4712a8e
choigonyok <achoistic98@naver.com>
2023-07-23 00:47:00 +0900

    DESIGN : SEARCHWORD UX 수정

--- frontend/src/Searchword.js
+++ frontend/src/Searchword.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import "./Searchword.css";
 
 const Searchword = () => {
   const now = new Date();
@@ -58,7 +59,7 @@ const Searchword = () => {
         }
       })
       .catch((error) => {
-          console.log(error);
+        console.log(error);
       });
   };
 
@@ -89,14 +90,23 @@ const Searchword = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="검색할 단어를 입력하세요"
-        value={inputWord}
-        onChange={inputWordHandler}
-      />
-      <input type="button" value="검색" onClick={sendWordHandler} />
+    <div className="searchword-container">
+      <div className="searchword-container__seperate">
+        <input
+          type="text"
+          placeholder="검색할 단어를 입력하세요"
+          value={inputWord}
+          onChange={inputWordHandler}
+          className="searchword-input"
+        />
+        <input
+          type="button"
+          value="검색"
+          onClick={sendWordHandler}
+          className="searchword-button"
+        />
+      </div>
+
       <div>
         {searchedWords.length === 1 && (
           <div>
@@ -123,68 +133,78 @@ const Searchword = () => {
           </div>
         )}
       </div>
-      <div>
-        <select name="select" id="year" onChange={yearHandler}>
-          <option value="2023">2023</option>
-          <option value="2024">2024</option>
-          <option value="2025">2025</option>
-        </select>
-        <select name="select" id="month" onChange={monthHandler}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-        <select name="select" id="date" onChange={dateHandler}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18">18</option>
-          <option value="19">19</option>
-          <option value="20">20</option>
-          <option value="21">21</option>
-          <option value="22">22</option>
-          <option value="23">23</option>
-          <option value="24">24</option>
-          <option value="25">25</option>
-          <option value="26">26</option>
-          <option value="27">27</option>
-          <option value="28">28</option>
-          <option value="29">29</option>
-          <option value="30">30</option>
-          <option value="31">31</option>
-        </select>
+      <div className="searchword-container__seperate">
+        <div>
+          <select name="select" id="year" onChange={yearHandler}>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+          </select>
+          <select name="select" id="month" onChange={monthHandler}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+          <select name="select" id="date" onChange={dateHandler}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+            <option value="25">25</option>
+            <option value="26">26</option>
+            <option value="27">27</option>
+            <option value="28">28</option>
+            <option value="29">29</option>
+            <option value="30">30</option>
+            <option value="31">31</option>
+          </select>
+        </div>
+        <div>
+          <input
+            type="button"
+            value="검색"
+            onClick={sendDateHandler}
+            className="searchword-button"
+          />
+        </div>
       </div>
+
       <div>
-        <input type="button" value="검색" onClick={sendDateHandler} />
-      </div>
-      <div>
-        {searchedDate.length && <div>
-          {searchedDate[0].text_body}
-          {searchedDate[0].write_time}
-          </div>}
+        {searchedDate.length !== 0 && (
+          <div>
+            {searchedDate[0].text_body}
+            {searchedDate[0].write_time}
+          </div>
+        )}
       </div>
     </div>
   );
